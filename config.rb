3cc89fb2ab0c8f6feb4cb34a2ec94039d2387004
Richard Ber Chapman <berchapman@gmail.com>
2015-07-19 10:29:34 +0000

    initial

---
+++ config.rb
@@ -0,0 +1,17 @@
+# Compass CSS framework config file
+require 'breakpoint'
+
+
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "sass"
+css_dir = "style"
+images_dir = "assets"
+javascripts_dir = "js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :compressed
+relative_assets = true
